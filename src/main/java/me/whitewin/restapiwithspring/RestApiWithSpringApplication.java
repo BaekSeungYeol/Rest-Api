7a752fcd7baa360dc751053cb663d6c4800b0bf7
BaekSeungYeol <beck33333@naver.cm>
2019-12-27 00:27:15 +0900

    project creation

---
+++ src/main/java/me/whitewin/restapiwithspring/RestApiWithSpringApplication.java
@@ -0,0 +1,11 @@
+package me.whitewin.restapiwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWithSpringApplication.class, args);
+    }
+}
