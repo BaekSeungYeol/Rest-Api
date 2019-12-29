4aa1654a98b1b48ded05e840456c20a9e06a4e14
BaekSeungYeol <beck33333@naver.cm>
2019-12-29 23:39:33 +0900

    validate wrong data

--- src/test/java/me/whitewin/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/whitewin/restapiwithspring/events/EventControllerTests.java
@@ -110,5 +110,28 @@ public class EventControllerTests {
                 .content(this.objectMapper.writeValueAsString(eventDto)))
                 .andExpect(status().isBadRequest());
     }
+
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception
+    {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("Rest Api Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 12, 26, 12, 11))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 12, 25, 12, 11))
+                .beginEventDateTime(LocalDateTime.of(2019, 12, 24, 12, 30))
+                .endEventDateTime(LocalDateTime.of(2019, 12, 23, 12, 30))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("노원역 D2 팩토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
 }
 
