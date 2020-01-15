672c26028d39bae28d65fc6f934b2da9b06d9b3e
BaekSeungYeol <beck33333@naver.com>
2020-01-15 17:50:24 +0900

    Test exceptions

--- src/test/java/me/whitewin/restapiwithspring/accounts/AccountServiceTest.java
+++ src/test/java/me/whitewin/restapiwithspring/accounts/AccountServiceTest.java
@@ -1,12 +1,16 @@
 package me.whitewin.restapiwithspring.accounts;
 
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
@@ -17,6 +21,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest
 // @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -44,6 +51,19 @@ public class AccountServiceTest {
         //Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
     }
+
+    @Test
+    public void findByUserNameFail() {
+
+        // Expected
+        String username = "random@email.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
+    }
+
 
 
 }
