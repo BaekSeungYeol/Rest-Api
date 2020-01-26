4995716155753521e308ec9c87dde1a919d7a1ab
BaekSeungYeol <beck33333@naver.com>
2020-01-27 02:27:41 +0900

    Manager only shows Id

--- src/main/java/me/whitewin/restapiwithspring/events/Event.java
+++ src/main/java/me/whitewin/restapiwithspring/events/Event.java
@@ -1,7 +1,9 @@
 package me.whitewin.restapiwithspring.events;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 import me.whitewin.restapiwithspring.accounts.Account;
+import me.whitewin.restapiwithspring.accounts.AccountSerializer;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -29,6 +31,7 @@ public class Event {
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
     @ManyToOne
+    @JsonSerialize(using = AccountSerializer.class)
     private Account manager;
 
     public void update() {
